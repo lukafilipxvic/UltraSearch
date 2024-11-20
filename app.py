50eef11c3b47f89ffcb3d8668bf22cc186057d47
Luka Filipovic <57617694+lukafilipxvic@users.noreply.github.com>
2024-11-21 10:39:36 +1100

    libgen-enhanced

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 import streamlit as st
+import asyncio
 import random
 import base64
-from bs4 import BeautifulSoup
-from libgen_api import LibgenSearch
+from libgen_api_modern import LibgenSearch
 import pandas as pd
-import stealth_requests as requests
 from tools.analytics import inject_ga
-from tools.libgen_image import libgen_image
+from tools.image_downloader import async_download_image
 import tools.hide_st as hide_st
 
 st.set_page_config(
@@ -32,22 +31,19 @@ colA, colB, colC = st.columns([0.2, 0.6, 0.2])
 data_url = rnd_image_load()
 colB.markdown(
     f'''
-    <img src="data:image/gif;base64,{data_url}" alt="UltraSearch Logo" style="width: 100%; height: auto; margin-top: calc(20%); margin-bottom: 5%;">
+    <img src="data:image/gif;base64,{data_url}" alt="UltraSearch Logo" style="width: 100%; height: auto; margin-top: calc(15%); margin-bottom: 10%;">
     ''',
     unsafe_allow_html=True)
 colB.write()
 
 s = LibgenSearch()
 
-def search_books(search_type, query):
-    if search_type == "Author" and filters:
-        results = s.search_author_filtered(query, filters, exact_match=False)
-    elif search_type == "Author" and not filters:
-        results = s.search_author(query)
-    elif search_type == "Book Title" and filters:
-        results = s.search_title_filtered(query, filters, exact_match=False)
-    elif search_type == "Book Title" and not filters:
-        results = s.search_title(query)
+async def search_books(search_type: str, query: str, file_type: str, english_only: bool) -> list[dict]:
+    filters = {}
+    if search_type in ["Author", "Title"] and filters:
+        results = await s.search_filtered(query, filters, search_type=search_type.lower(), exact_match=False)
+    elif search_type in ["Author", "Title"] and not filters:
+        results = await s.search(query, search_type=search_type.lower())
 
     filtered_results = []
     for book in results:
@@ -55,70 +51,69 @@ def search_books(search_type, query):
             filtered_results.append(book)
     return filtered_results
 
-def resolve_download_links(item):
-    MIRROR_SOURCES = ["GET", "Cloudflare", "IPFS.io", "Pinata"]
-    mirror_1 = item["Mirror_1"]
-    page = requests.get(mirror_1)
-    soup = BeautifulSoup(page.text, "html.parser")
-    links = soup.find_all("a", string=MIRROR_SOURCES)
-    download_links = {link.string: link["href"] for link in links}
-    return download_links
+async def download_image(url: str):
+    return await async_download_image(url)
 
-def display_results(results):
+async def display_results(results):
     for i, book in enumerate(results, start=1):
         with st.spinner(f'Gathering knowledge... &emsp; {i}/{len(results)}'):
-            download_links = resolve_download_links(book)
-            image_path = libgen_image(book)
-
+            image_path = await download_image(book['Cover'])
             # Columns for response
             left, mid, right = st.columns([0.8, 3, 1], gap="small")
             left.image(image_path)
 
             mid.caption(f"**Year:** {book['Year']}&emsp;**Pages:** {book['Pages']}&emsp;**Size:** {book['Size']}&emsp;**Extension**: {book['Extension']}")
-            mid.write(f"[**{book['Title']}**]({list(download_links.values())[0]})")
-            mid.write(f"*{book['Author']}*")
+            mid.write(f"[**{book['Title']}**]({book['Direct Download Link 1']})")
+            mid.write(f"*{book['Author(s)']}*")
 
             # Display the download links as "Link 1", "Link 2", "Link 3"
-            for i, value in enumerate(download_links.values(), start=1):
-                right.markdown(f"[Download Link {i}]({value})")
+            for i in range(1, 4):  # Loop through 1, 2, 3
+                key = f"Direct Download Link {i}"  # Construct the key dynamically
+                if key in book:  # Check if the key exists in the book dictionary
+                    right.markdown(f"[Download Link {i}]({book[key]})")
             st.divider()
 
-col1, col2, col3 = st.columns([0.32, 0.45, 0.2], gap="small")
+async def main():
+    filters = {}
+    col1, col2, col3 = st.columns([0.32, 0.52, 0.18], gap="small")
 
-search_type = col1.radio("Search by:", ["Book Title", "Author"], index=0, horizontal=True, label_visibility="collapsed")
-file_type = col2.radio('File Extension:', ['Any', 'EPUBs only', 'PDFs only'], index=0, horizontal=True, label_visibility="collapsed")
-english_only = col3.checkbox('English Only', value=True)
+    search_type = col1.radio("Search by:", ["Title", "Author"], index=0, horizontal=True, label_visibility="collapsed")
+    file_type = col2.radio('File Extension:', ['Any', 'EPUBs only', 'PDFs only'], index=0, horizontal=True, label_visibility="collapsed")
+    english_only = col3.checkbox('English Only', value=True)
 
-query = st.text_input(label=f"Search {search_type.lower()}:",
-                        placeholder=f"Search {search_type.lower()}",
-                        help="Search is case and symbol sensitive.",
-                        label_visibility="collapsed")
+    colX, colY = st.columns([0.999, 0.001], gap="small")
+
+    query = colX.text_input(label=f"Search {search_type}:",
+                            placeholder=f"Search {search_type}",
+                            help="Search is case and symbol sensitive.",
+                            label_visibility="collapsed")
+
+    if file_type == 'EPUBs only':
+        filters['Extension'] = 'epub'
+    elif file_type == 'PDFs only':
+        filters['Extension'] = 'pdf'
+    if english_only:
+        filters['Language'] = 'English'
+
+    if colY.button("🔎"):
+        with st.spinner('Searching...'):
+            results = await search_books(search_type, query, file_type, english_only)
+        if results:
+            #st.write(results)
+            #st.dataframe(results, use_container_width=True) ## df of the results
+
+            new_results = pd.DataFrame(results)
+            new_results.fillna({'Year':'NA'}, inplace=True)
+
+            st.info(f"Showing results for {len(results)} items")
+            st.divider()
+            await display_results(results)
+        else:
+            st.info("None found. Please try again.")
 
 
-filters = {}
+    hide_st.header()
+    hide_st.footer()
 
-if file_type == 'EPUBs only':
-    filters['Extension'] = 'epub'
-elif file_type == 'PDFs only':
-    filters['Extension'] = 'pdf'
-if english_only:
-    filters['Language'] = 'English'
-
-if st.button("Search"):
-    with st.spinner('Searching...'):
-        results = search_books(search_type, query)
-    if results:
-        new_results = pd.DataFrame(
-            results, columns=['Author', 'Title', 'Publisher', 'Year', 'Size', 'Extension'])
-        new_results.fillna({'Year':'NA'}, inplace=True)
-
-        st.info(f"Showing results for {len(results)} items")
-        # st.dataframe(new_results, use_container_width=True) ## df of the results
-        st.divider()
-        display_results(results)
-    else:
-        st.info("None found. Please try again.")
-
-
-hide_st.header()
-hide_st.footer()
+if __name__ == "__main__":
+    asyncio.run(main())
